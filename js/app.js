591948bd31bbb510825ca258dcb8a5e293cf34d6
Garrison Benson <farmcodegary@protonmail.com>
2017-04-10 17:55:27 -0400

    Added initial version of survey. (Does not yet email scores.)

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
 $(document).foundation();
+
+function surveyResultToScore(result) {
+  switch(result) {
+    case "Strongly agree":
+      return 2;
+    case "Agree":
+      return 1;
+    case "Neutral":
+      return 0;
+    case "Disagree":
+      return -1;
+    case "Strongly disagree":
+      return -2;
+    default:
+      console.error("Erroneous survey result: "+$(this).val());
+  }
+}
+
+$("#surveyForm").submit(function(event) {
+  var total = 0;
+  $(".positive .surveyQuestion").each(function() {
+    var value = surveyResultToScore($(this).val());
+    total += value;
+  });
+  $(".negative .surveyQuestion").each(function() {
+    var value = surveyResultToScore($(this).val());
+    total -= value;
+  });
+  if (total > 0) {
+    $('#surveyModalYes').foundation('open');
+  }
+  else {
+    $('#surveyModalNo').foundation('open');
+  }
+  event.preventDefault();
+});
